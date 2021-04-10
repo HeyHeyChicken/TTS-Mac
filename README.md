dbaec62d9bc0d0abf4aea6c7b7888e07c405fae5
HeyHeyChicken <antoine@cuffel.fr>
2021-04-10 13:07:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<div align="center">
+<a href="//nova-assistant.com" rel="nofollow">
+<img src="https://github.com/HeyHeyChicken/NOVA-HomePod-Sounds/blob/master/resources/nova-logo.svg" alt="NOVA" width="300">
+</a>
+
+**NOVA - TTS - Mac** is a skill for your [NOVA](//github.com/HeyHeyChicken/NOVA) assistant.<br>
+This skill will allow your assistant to answer you (no connection needed) with the Mac's Siri voice.
+</div>
+
+<br>
+
+## Installation
+
+1) Go to your NOVA's server UI (the default URL is "http://localhost:8080/")
+2) Go to the "Skills" tab.
+3) Find the "TTS - Mac" skill, open the modal and click on the "Install" button.
+
+## Features
+
+This skill will allow your assistant to answer you (no connection needed) with the Mac's Siri voice.
+/!\ You need to have your server installed on a Mac.
+
+## Support and contribution
+
+I provide support for all users through [GitHub issues](//github.com/HeyHeyChicken/NOVA-TTS-Mac/issues).
+
+If you would like to contribute to this project, make sure you first read the [guide for contributors](//github.com/HeyHeyChicken/NOVA/blob/master/CONTRIBUTING.md).
+
+<br>
+<br>
+
+Created by [Antoine Duval (HeyHeyChicken)](//antoine.cuffel.fr) with ❤ and ☕ (chocolate) in [Déville-lès-Rouen](//en.wikipedia.org/wiki/Déville-lès-Rouen).
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const LIBRARIES = {
+    FS: require("fs"),
+    NodeCMD: require("node-cmd"),
+    Path: require("path"),
+
+    Skill: require("../../../Libraries/Skill"),
+    Audio: require("../../../Audio")
+};
+
+class Random extends LIBRARIES.Skill{
+    constructor(_main, _settings) {
+        super(_main, _settings);
+
+        this.RootPath = _main.DirName + "/lib/skills/HeyHeyChicken_NOVA-TTS-Mac/src/";
+
+        const SELF = this;
+
+        this.Main.TTS = function (_socket, _text, _callback){
+            if(_text !== undefined){
+                const FILE_NAME = "/MacTTS-" + new Date().getTime();
+                const ABSOLUTE_PATH = LIBRARIES.Path.resolve(SELF.Main.DirName + "/public/mp3/TTS/") + FILE_NAME;
+                const LOCAL_PATH = LIBRARIES.Path.resolve( "/mp3/TTS/") + FILE_NAME;
+                const COMMAND = "say -o \"" + ABSOLUTE_PATH + ".aiff\" \"" + _text + "\"";
+                LIBRARIES.NodeCMD.get(
+                    COMMAND,
+                    function(err, data, stderr){
+                        // On convertis le fichier AIFF en MP3
+                        const FFMPEG_ABSOLUTE_PATH = LIBRARIES.Path.resolve(SELF.RootPath + "ffmpeg");
+                        const CONVERT_COMMAND = FFMPEG_ABSOLUTE_PATH + " -i " + ABSOLUTE_PATH + ".aiff -f mp3 -acodec libmp3lame -ab 192000 -ar 44100 " + ABSOLUTE_PATH + ".mp3";
+                        LIBRARIES.NodeCMD.get(
+                            CONVERT_COMMAND,
+                            function(err, data, stderr){
+                                // ON SUPPRIME l'ANCIEN FICHIER AIFF
+                                LIBRARIES.FS.unlink(ABSOLUTE_PATH + ".aiff", function(){
+                                    _socket.emit("play_audio", [new LIBRARIES.Audio(LOCAL_PATH + ".mp3", 1)]);
+                                    if(_callback !== undefined){
+                                        _callback(_text);
+                                    }
+                                });
+                            }
+                        );
+                    }
+                );
+            }
+        }
+    }
+}
+
+module.exports = Random;
